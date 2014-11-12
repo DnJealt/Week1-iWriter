565db534b695d0e6b0a0afb7dfd5de5c8cc79cfb
Rick van Osta <osta1995@hotmail.com>
2014-11-12 12:47:25 +0100

    kont

---
+++ Writer/Writer/WriterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Writer
+{
+    class WriterFactory
+    {
+        List<IWriter> Writers = new List<IWriter>();
+
+        public IWriter WriterFactory(string writer)
+        {
+            switch (writer)
+            {
+                case("blue"):
+                    return new BlueWriter();
+                case ("red"):
+                    return new RedWriter();
+                case ("420"):
+                    return new _420Writer();
+                case ("rainbow"):
+                    return new RainbowWriter();
+            }
+        }
+    }
+}
